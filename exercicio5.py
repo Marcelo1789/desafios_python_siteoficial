447b697e0ed43db1fa811721c9bb3ba3d50a8778
Marcelo1789 <marcelo1789@hotmail.com>
2022-06-13 18:40:52 -0300

    2 +

---
+++ exercicio5.py
@@ -0,0 +1,3 @@
+metric = float(input('Digite sua medida em metros: '))
+centimeter = (metric*100)
+print ('Sua medida em centimetros é: ', centimeter, 'CM')
--- exercico4.py
+++ exercico4.py
@@ -2,5 +2,5 @@ nota1 = float(input('Digite sua nota do primeiro bimestre: '))
 nota2 = float(input('Digite sua nota do segundo bimestre: '))
 nota3 = float(input('Digite sua nota do terceiro bimestre: '))
 nota4 = float(input('Digite sua nota do quarto bimestre: '))
-media = (nota1 + nota2 + nota3 + nota4)/2
+media = (nota1 + nota2 + nota3 + nota4)/4
 print ('Sua media foi de: ', media)
